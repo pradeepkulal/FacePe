d65a1073bc7a0b02f2f77455d65b72a4c4237ea1
pradeep <pradeep@intuitiveapps.com>
2025-01-16 16:13:35 +0530

    first

--- src/test/java/IntuitiveApps/sample/AppTest.java
+++ src/test/java/IntuitiveApps/sample/AppTest.java
@@ -3,23 +3,39 @@ package IntuitiveApps.sample;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class AppTest {
-	@Test
-	public void appiumTest() {
-		UiAutomator2Options options =  new UiAutomator2Options();
-		options.setDeviceName("R58N345JQXY");
-		options.setApp("D:\\eclipse-workspace\\FacePe\\resources\\General-Store.apk");
-		AndroidDriver driver;
-		try {
-			driver =  new AndroidDriver(new URL("http://0.0.0.0:4723"),options);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    private AndroidDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("R58N345JQXY"); // Replace with your device name
+        options.setApp("D:\\eclipse-workspace\\FacePe\\resources\\General-Store.apk"); // Path to your app
+
+        try {
+            driver = new AndroidDriver(new URL("http://192.168.31.103:4723/"), options); // Ensure correct URL format
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Appium server URL is invalid", e);
+        }
+    }
+
+    @Test
+    public void appiumTest() {
+        // Your test code goes here
+        System.out.println("Appium test started...");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+        //    driver.quit(); // Close the driver to free resources
+        }
+    }
 }
