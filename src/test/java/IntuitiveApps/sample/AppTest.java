2617ff604c3d8957ef3a932a4ae7af9fa036e772
unknown <pradeep@intuitiveapps.com>
2025-01-17 18:44:41 +0530

    added emulator

--- src/test/java/IntuitiveApps/sample/AppTest.java
+++ src/test/java/IntuitiveApps/sample/AppTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -21,7 +22,7 @@ public class AppTest {
 
     @BeforeTest
     public void setup() {
-        final String MAIN_JS_PATH = "C:\\Users\\USER\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+        final String MAIN_JS_PATH = System.getProperty("user.home")+"\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
         final String APPIUM_HOME = "C:\\Users\\USER\\AppData\\Roaming\\npm\\node_modules\\appium"; // Custom Appium home directory
         final String IP_Address = "192.168.31.103";
         final int PORT = 4723;
@@ -41,8 +42,9 @@ public class AppTest {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("ANDROID");
         options.setAutomationName("UIAutomator2");
-        options.setDeviceName("R58N345JQXY"); // Replace with actual device name from `adb devices`
-        options.setApp("D:\\eclipse-workspace\\FacePe\\resources\\General-Store.apk"); // Path to your app
+      //  options.setDeviceName("R58N345JQXY");
+        options.setDeviceName("emulator-5554");// Replace with actual device name from `adb devices`
+        options.setApp("D:\\eclipse-workspace\\FacePe\\resources\\ApiDemos-debug.apk"); // Path to your app
 
         try {
             // Ensure the driver is created after the service starts
@@ -50,6 +52,8 @@ public class AppTest {
         } catch (MalformedURLException e) {
             throw new RuntimeException("Appium server URL is invalid", e);
         }
+        
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
     }
 
     @Test
@@ -61,10 +65,12 @@ public class AppTest {
     @AfterTest
     public void tearDown() {
         if (driver != null) {
-            driver.quit(); // Close the driver to free resources
+    //        driver.quit(); // Close the driver to free resources
         }
         if (service != null && service.isRunning()) {
             service.stop(); // Stop the service
         }
     }
+    
+    
 }
